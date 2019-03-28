5aae526964714d73e3db261f1f4ded25ea0c3074
Salah GHAMIZI <salah.ghamizi@uni.lu>
2019-03-28 14:36:58 +0100

    Updated the predictor+ LeNet5 Block implementation

--- full_lenet5.py
+++ full_lenet5.py
@@ -1,51 +1,52 @@
-
 from tensorflow_generator import TensorflowGenerator
 from products_tree import ProductSet
 import multiprocessing
 import json
 
-baseurl = "./"
-url = "100Products_lenet5_constrained"
-productSet = ProductSet(baseurl+url+".pdt")
-
-min_index = 0
 
 
-def run_tensorflow(product):
-        datasets = ["mnist"]
+
+def run_tensorflow(product, url, index,datasets=[], epochs=12, depth=1, data_augmentation=False):
         for dataset in datasets:
-                tensorflow = TensorflowGenerator(product,12, dataset)
-                f2 = open("reports/report_{0}_{1}.txt".format(url,dataset),"a")
-                f2.write("\r\n{0}: {1} {2} {3} {4} {5}".format(index, tensorflow.accuracy, tensorflow.stop_training, tensorflow.training_time, tensorflow.params, tensorflow.flops))
+                logpath = "report_top_{2}epochs_{3}depth_{0}_{1}.txt".format(url,dataset, epochs, depth)
+                tensorflow = TensorflowGenerator(product,epochs, dataset, depth=depth, data_augmentation=data_augmentation)
+                f2 = open(logpath,"a")
+
+                history = "{accuracy}|{validation_accuracy}".format(accuracy="#".join(map(str, tensorflow.history[0])), validation_accuracy="#".join(map(str, tensorflow.history[1])))
+                f2.write("\r\n{0}: {1} {2} {3} {4} {5} {6}".format(index, tensorflow.accuracy, tensorflow.stop_training, tensorflow.training_time, tensorflow.params, tensorflow.flops, history))
                 f2.close()
 
-for index,product in enumerate(productSet.format_products()):
-    print("product {0}".format(index))
+def main(target, min_index=0, max_index=0, filter_indices=[], datasets=None,epochs=12, depth=1, data_augmentation=False):
+        baseurl = "./"
+        productSet = ProductSet(baseurl+target+".pdt")
 
-    if index >= min_index:
-        f = open("{0}products/{1}_{2}.json".format(baseurl, url, index), "w")
-        str_ = json.dumps(product)
-        f.write(str_)
-        f.close()
+        if not datasets:
+                datasets = ["mnist"]
 
-        run_tensorflow(product)
-        #p = multiprocessing.Process(target=run_tensorflow, args=(product,))
-        #p.start()
-        #p.join()
+        for index,product in enumerate(productSet.format_products()):
+                print("product {0}".format(index))
 
-       
+                if index >= min_index and (len(filter_indices)==0 or index in filter_indices):
+                        f = open("{0}products/{1}_{2}.json".format(baseurl, target, index), "w")
+                        str_ = json.dumps(product)
+                        f.write(str_)
+                        f.close()
 
-        
-       
-        
-        
+                        run_tensorflow(product, target, index, datasets, epochs, depth, data_augmentation=data_augmentation)
+                        #p = multiprocessing.Process(target=run_tensorflow, args=(product,))
+                        #p.start()
+                        #p.join()
 
-        
+                if max_index!= 0 and index ==max_index:
+                        break
 
-        
+if __name__ == "__main__":
+    # execute only if run as a script
 
+    top_cifar = [59, 63, 143,  161, 203, 444, 477, 595, 634,  936]
+    top_cifar = []
+    # small architectures < 4M on CIFAR
+    #top_cifar = [59, 203, 477, 634, 63, 161, 936]
+    #main("100Products_lenet5_constrained", datasets=["cifar"], filter_indices=top_cifar,epochs=300, depth=1, data_augmentation=False, min_index=21)
 
-        
-
-
-
+    TensorflowGenerator("lenet5",12, "mnist", depth=1, data_augmentation=False)
--- predictor.py
+++ predictor.py
@@ -6,7 +6,7 @@ import keras
 class Predictor(object):
 
 
-    def __init__(self, url_results, url_features):
+    def __init__(self, url_results, url_features, url_tests=None):
         X = []
         Y = []
         f = open(url_results, 'r')
@@ -45,29 +45,70 @@ class Predictor(object):
         X = np.array(X).astype(int)
 
         #We split the Y values into 10 batchs
-        Y = np.floor(np.array(Y).astype(float) *num_classes).astype(int)
-        print(Y[0])
+        Y_batch = np.floor(np.array(Y).astype(float) *num_classes).astype(int)
 
-        Y =  keras.utils.to_categorical(Y, num_classes)
+        Y =  keras.utils.to_categorical(Y_batch, num_classes)
 
         model = Sequential()
-        model.add(Dense(128, input_dim=8847, activation='relu'))
-        model.add(Dense(128, activation='relu'))
+        model.add(Dense(32, input_dim=X[0].size, activation='relu'))
+        model.add(Dense(32, activation='relu'))
         model.add(Dense(num_classes, activation='softmax'))
         model.compile(loss="categorical_crossentropy", metrics=['accuracy'], optimizer="sgd")
 
-        X_train = X[0:split_elements]
-        Y_train = Y[0:split_elements]
-        model.fit(X_train, Y_train, epochs=100, verbose=1)
-        score = model.evaluate(X[split_elements:], Y[split_elements:])
+        if url_tests:
+            X_train = X
+            Y_train = Y
+            X_t = []
+            Y_t = []
+            f = open(url_results, 'r')
+            line = f.readline()
+            while line:
+                arch = line.split(" ")
+                if len(arch)>1:
+                    Y_t.append(float(arch[1]))
+                line = f.readline()
+            f.close()
 
-        print("predictor score {0}".format(score))
+            f = open(url_features, 'r')
+            line = f.readline()
+            while line:
+                
+                product_features = line.split(";")
+                
+                if len(product_features) > 1:
+                    product_features = product_features[:len(product_features)-1]
+                    product_features = [int(x) for x in product_features]
+                    product_features = [0 if x<0 else 1 for x in product_features]
+                    X_t.append(product_features) 
 
+                line = f.readline()
+            f.close()
+
+            Y_test =  keras.utils.to_categorical(np.floor(np.array(Y_t).astype(float) *num_classes).astype(int), num_classes)
+            X_test = X_t
         
-        print("predict {0} vs {1}".format(model.predict(X[split_elements:]), Y[split_elements:]))
+        else:
+            X_train = X[0:split_elements]
+            Y_train = Y[0:split_elements]
+            X_test = X[split_elements:]
+            Y_test =  Y[split_elements:]
+
+        model.fit(X_train, Y_train, epochs=100, verbose=1)
+        score = model.evaluate(X_test, Y_test)
+
+        prediction = model.predict(X_test)
+        print(np.argmax(prediction, axis=1),np.argmax(Y_test, axis=1))
+
+        #top = np.amax(prediction, axis=1)
+        #top_index = [np.where(prediction[index] == top_e)[0] for (index,top_e) in enumerate(top)]
+        #print(top, top_index)
+        print("predictor score {0}".format(score))      
+       # print("predict {0} {1}".format(np.argmax(prediction, axis=1), Y[split_elements:]))
         
 
  
 if __name__ == '__main__':
     
-    predictor = Predictor("./report_100.txt", "./100Products.pdt")
+    #predictor = Predictor("./reports/report_100.txt", "./100Products.pdt")
+    predictor = Predictor("./report1000Products_cifar.txt", "./1000Products.pdt")
+    
