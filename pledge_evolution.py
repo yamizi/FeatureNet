6ab0c3686f3340517437ca5905cd4fc69d254e45
Salah GHAMIZI <salah.ghamizi@uni.lu>
2019-05-16 17:22:09 +0200

    Evolution Epoch resumable + OOP Style

--- pledge_evolution.py
+++ pledge_evolution.py
@@ -15,83 +15,13 @@ import gc
 import datetime
 import time
 from math import ceil
+import re
 
 
 pledge_path = '../products/PLEDGE.jar'
 base_path = '../products'
 training_epochs = 25
 evolution_epochs = 20
-
-
-def run_pledge(input_file, nb_base_products, output_file):
-    params = ['java', '-jar', pledge_path, 'generate_products']
-    params = params+['-fm', input_file, '-nbProds',
-                     str(nb_base_products), '-o', output_file]
-    start = time.time()
-    print("running pledge {} on {}: {}".format(
-        pledge_path, params, datetime.datetime.now()))
-    pledge_result = subprocess.check_call(params)
-
-    end = time.time()
-    print("pledge result {} in {}s".format(pledge_result, str(end-start)))
-    return
-
-
-def select(last_population, survival_count):
-    labels = last_population.get("filtered_features_label")
-    products = last_population.get("products")
-    products = sorted(products, key=lambda x: x.accuracy,
-                      reverse=True)[:survival_count]
-
-    products_labels = [[labels[k] for k in labels.keys(
-    ) if prd.features[int(k)] == 1] for prd in products]
-    return products, products_labels
-
-
-def mutate(mutant_path, mutant_labels, initial_feature_model, nb_products, constraints_ratio=0.5):
-
-    mutant_labels = [
-        mutant for mutant in mutant_labels if random.random() < constraints_ratio]
-
-    print("### mutate product from {} to {}".format(initial_feature_model, mutant_path))
-    import xml.etree.ElementTree as ET
-    tree = ET.parse(initial_feature_model)
-    root = tree.getroot()
-    constraints = root.getchildren()[1]
-    constraints_raw = constraints.text.split("\n")
-    nb_constraints = len(constraints_raw)
-    constraints_raw = constraints_raw[:-1]
-    for i, lbl in enumerate(mutant_labels):
-        constraints_raw.append(
-            "C{}:~Architecture  or  ~{}".format(nb_constraints+i-1, lbl))
-
-    constraints.text = "\n".join(constraints_raw)
-    dst = "{}.xml".format(mutant_path)
-    tree.write(dst, encoding="UTF-8", xml_declaration=True)
-    #text = ET.tostring(root,"unicode" )
-    #f = open(dst, 'w')
-    #f.write(text)
-    #f.close()
-
-    if not os.path.isfile(dst):
-        print("file {} not saved yet".format(dst))
-        raise Exception()
-
-    output_file = "{}.{}".format(dst[:-4], "pdt")
-    run_pledge(dst, int(nb_products), output_file)
-
-    # possibility to generate multiple products each containing a different set of constraints
-    return output_file
-
-
-def extract_leaves(output_file):
-    initial_product_set = ProductSet(output_file)
-    filtered_features_label = ProductSet.filter_leaves(
-        initial_product_set.features)
-    last_population = {"filtered_features_label": [], "products": []}
-    last_population["filtered_features_label"] = filtered_features_label
-
-    return initial_product_set, last_population
 
 
 def reset_keras(classifier):
@@ -115,90 +45,100 @@ def reset_keras(classifier):
     #  config.gpu_options.visible_device_list = "0"
     set_session(tensorflow.Session(config=config))
 
-
-def train_products(initial_product_set, products_file, dataset):
+def run_pledge(input_file, nb_base_products, output_file):
+    params = ['java', '-jar', pledge_path, 'generate_products']
+    params = params+['-fm', input_file, '-nbProds',
+                    str(nb_base_products), '-o', output_file]
     start = time.time()
-    print("### training products in {} for dataset {}: {}".format(
-        products_file, dataset, datetime.datetime.now()))
-    last_population = []
-    for index, (product, original_product) in enumerate(initial_product_set.format_products()):
-        print("### training product {}".format(index))
-        tensorflow = TensorflowGenerator(product, training_epochs, dataset, product_features=original_product,
-                                         features_label=initial_product_set.features, no_train=False, data_augmentation=False)
+    print("running pledge {} on {}: {}".format(
+        pledge_path, params, datetime.datetime.now()))
+    pledge_result = subprocess.check_call(params)
 
-        if tensorflow and hasattr(tensorflow,"model") and tensorflow.model:
-            last_population.append(tensorflow.model.to_vector())
-            
-        reset_keras(tensorflow)
     end = time.time()
-    print("### training products over, took {}s".format(str(end-start)))
-    return last_population
+    print("pledge result {} in {}s".format(pledge_result, str(end-start)))
+    return
+    
+class PledgeEvolution(object):
 
+    
+    @staticmethod
+    def select(last_population, survival_count):
+        labels = last_population.get("filtered_features_label")
+        products = last_population.get("products")
+        products = sorted(products, key=lambda x: x.accuracy,
+                        reverse=True)[:survival_count]
 
-def run(base_path, input_file="", output_file="", last_pdts_path="", nb_base_products=100, dataset="cifar"):
+        products_labels = [[labels[k] for k in labels.keys(
+        ) if prd.features[int(k)] == 1] for prd in products]
+        return products, products_labels
 
-    if not os.path.isdir(base_path):
-        os.mkdir(base_path)
+    @staticmethod
+    def mutate(mutant_path, mutant_labels, initial_feature_model, nb_products, constraints_ratio=0.5):
 
-    session_path = "{}/{}".format(base_path, dataset)
+        mutant_labels = [
+            mutant for mutant in mutant_labels if random.random() < constraints_ratio]
 
-    if not os.path.isdir(session_path):
-        os.mkdir(session_path)
+        print("### mutate product from {} to {}".format(initial_feature_model, mutant_path))
+        import xml.etree.ElementTree as ET
+        tree = ET.parse(initial_feature_model)
+        root = tree.getroot()
+        constraints = root.getchildren()[1]
+        constraints_raw = constraints.text.split("\n")
+        nb_constraints = len(constraints_raw)
+        constraints_raw = constraints_raw[:-1]
+        for i, lbl in enumerate(mutant_labels):
+            constraints_raw.append(
+                "C{}:~Architecture  or  ~{}".format(nb_constraints+i-1, lbl))
 
-    survival_count = int(0.1*nb_base_products)
-    nb_product_perparent =  int(0.9*nb_base_products)
-    last_evolution_epoch = 0
+        constraints.text = "\n".join(constraints_raw)
+        dst = "{}.xml".format(mutant_path)
+        tree.write(dst, encoding="UTF-8", xml_declaration=True)
+        #text = ET.tostring(root,"unicode" )
+        #f = open(dst, 'w')
+        #f.write(text)
+        #f.close()
 
-    if not input_file:
-        input_file = '{}/main_5_5.xml'.format(base_path)
-        #input_file = '{}main_1block_10_cells.xml'.format(base_path)
+        if not os.path.isfile(dst):
+            print("file {} not saved yet".format(dst))
+            raise Exception()
 
-    if not output_file:
-        output_file = '{}/{}products.pdt'.format(base_path, nb_base_products)
-        #output_file = '{}main_1blocks_10cells_{}products.pdt'.format(base_path,nb_base_products)
+        output_file = "{}.{}".format(dst[:-4], "pdt")
+        run_pledge(dst, int(nb_products), output_file)
 
-    if not last_pdts_path:
-        p = output_file.find("products")
-        last_pdts_path = "{}/{}products.json".format(
-            session_path, nb_base_products)
-        #output_file = '{}main_1blocks_10cells_{}products.pdt'.format(base_path,nb_base_products)
+        # possibility to generate multiple products each containing a different set of constraints
+        return output_file
 
-    if os.path.isfile(output_file):
-        print("Skipping initial PLEDGE run, file found in {}".format(output_file))
-    else:
-        print("Initial PLEDGE run")
-        run_pledge(input_file, nb_base_products, output_file)
+    @staticmethod
+    def extract_leaves(output_file):
+        initial_product_set = ProductSet(output_file)
+        filtered_features_label = ProductSet.filter_leaves(
+            initial_product_set.features)
+        last_population = {"filtered_features_label": [], "products": []}
+        last_population["filtered_features_label"] = filtered_features_label
 
-    product_set, last_population = extract_leaves(output_file)
+        return initial_product_set, last_population
 
-    if os.path.isfile(last_pdts_path):
-        print("Skipping initial training")
-        f1 = open(last_pdts_path, 'r')
-        vects = json.loads(f1.read())
-        last_population["products"] = [
-            KerasFeatureVector.from_vector(vect) for vect in vects]
+    @staticmethod
+    def train_products(initial_product_set, products_file, dataset):
+        start = time.time()
+        print("### training products in {} for dataset {}: {}".format(
+            products_file, dataset, datetime.datetime.now()))
+        last_population = []
+        for index, (product, original_product) in enumerate(initial_product_set.format_products()):
+            print("### training product {}".format(index))
+            tensorflow = TensorflowGenerator(product, training_epochs, dataset, product_features=original_product,
+                                            features_label=initial_product_set.features, no_train=False, data_augmentation=False)
 
-    else:
-        pop = train_products(product_set, output_file, dataset)
-        last_population["products"] = last_population["products"] + pop
-        f1 = open(last_pdts_path, 'w')
-        f1.write(json.dumps([i.to_vector()
-                             for i in last_population["products"]]))
-        f1.close()
+            if tensorflow and hasattr(tensorflow,"model") and tensorflow.model:
+                last_population.append(tensorflow.model.to_vector())
+                
+            reset_keras(tensorflow)
+        end = time.time()
+        print("### training products over, took {}s".format(str(end-start)))
+        return last_population
 
-    # list of (mutant_ratio,mutant_prds) sum of count_ratio should equal 1
-    mutant_ratios = [(1, 0.2),  (0.5, 0.2), (0.5, 0.2), (0.5, 0.2), (0.25, 0.2)]
-    #mutant_ratios = ((0.5, 1),)
-
-    for evo in range(evolution_epochs):
-        # The more we increase the epochs, the more we keep all the leaves
-        mutant_ratios[0] = (1, 0.2 +0.8*evo/(evolution_epochs-1))
-        mutant_ratios[1] = mutant_ratios[2] = mutant_ratios[3] =(0.5, 0.2*(evolution_epochs-evo)/evolution_epochs)
-
-        print("### evolution epoch {}".format(evo+last_evolution_epoch))
-        new_pop, new_pop_labels = select(last_population, survival_count)
-        last_population["products"] = new_pop
-        print("### remaining top individuals {}".format(len(new_pop)))
+    @staticmethod
+    def evolve(survival_count,mutant_ratios, evo, session_path, new_pop_labels,input_file, nb_product_perparent, product_set, dataset, last_population  ):
         for i in range(survival_count):
 
             print("### batch of product parent {}".format(i))
@@ -206,7 +146,7 @@ def run(base_path, input_file="", output_file="", last_pdts_path="", nb_base_pro
             for batch_id, (ratio, nb_prd) in enumerate(mutant_ratios):
                 mutant_id = "e{}_m{}_b{}".format(evo, i, batch_id)
                 mutant_path = "{}/{}".format(session_path, mutant_id)
-                mutants_pdts_path = mutate(mutant_path, new_pop_labels[i], input_file, max(3,ceil(nb_prd*nb_product_perparent)), ratio)
+                mutants_pdts_path = PledgeEvolution.mutate(mutant_path, new_pop_labels[i], input_file, max(3,ceil(nb_prd*nb_product_perparent)), ratio)
                 print("### batch of pledge sub-products {}".format(mutants_pdts_path))
                 try:
                     product_set.load_products_from_url(mutants_pdts_path)
@@ -214,7 +154,7 @@ def run(base_path, input_file="", output_file="", last_pdts_path="", nb_base_pro
                     print(e)
                     continue
 
-                pop = train_products(product_set, mutants_pdts_path, dataset)
+                pop = PledgeEvolution.train_products(product_set, mutants_pdts_path, dataset)
                 pop = sorted(pop, key=lambda x: x.accuracy, reverse=True)
                 f1 = open("{}.json".format(mutant_path), 'w')
 
@@ -223,16 +163,90 @@ def run(base_path, input_file="", output_file="", last_pdts_path="", nb_base_pro
 
                 last_population["products"] = last_population["products"] + pop
 
-        pop = sorted(last_population["products"],
-                     key=lambda x: x.accuracy, reverse=True)
-       
-        pdt_path = "{}/{}products_e{}.json".format(
-            session_path, nb_base_products, evo)
-        print("### remaining total individuals {} saved to {}. top accuracy: {}".format(
-            len(pop),pdt_path, pop[0].accuracy))
-        f1 = open(pdt_path, 'w')
-        f1.write(json.dumps([i.to_vector() for i in pop]))
-        f1.close()
+        return last_population
+
+    @staticmethod
+    def run(base_path, input_file="", output_file="", last_pdts_path="", nb_base_products=100, dataset="cifar"):
+
+        if not os.path.isdir(base_path):
+            os.mkdir(base_path)
+
+        session_path = "{}/{}".format(base_path, dataset)
+
+        if not os.path.isdir(session_path):
+            os.mkdir(session_path)
+
+        survival_count = int(0.1*nb_base_products)
+        nb_product_perparent =  int(0.9*nb_base_products)
+        last_evolution_epoch = 0
+
+        if not input_file:
+            input_file = '{}/main_5_5.xml'.format(base_path)
+            #input_file = '{}main_1block_10_cells.xml'.format(base_path)
+
+        if not output_file:
+            output_file = '{}/{}products.pdt'.format(base_path, nb_base_products)
+            #output_file = '{}main_1blocks_10cells_{}products.pdt'.format(base_path,nb_base_products)
+
+        if not last_pdts_path:
+            p = output_file.find("products")
+            last_pdts_path = "{}/{}products.json".format(
+                session_path, nb_base_products)
+            #output_file = '{}main_1blocks_10cells_{}products.pdt'.format(base_path,nb_base_products)
+
+        pattern = 'products_(\d+).json'
+        result = re.findall(pattern, last_pdts_path) 
+        if len(result):
+            last_evolution_epoch = int(result[0])+1
+
+        if os.path.isfile(output_file):
+            print("Skipping initial PLEDGE run, file found in {}".format(output_file))
+        else:
+            print("Initial PLEDGE run")
+            run_pledge(input_file, nb_base_products, output_file)
+
+        product_set, last_population = PledgeEvolution.extract_leaves(output_file)
+
+        if os.path.isfile(last_pdts_path):
+            print("Skipping initial training")
+            f1 = open(last_pdts_path, 'r')
+            vects = json.loads(f1.read())
+            last_population["products"] = [
+                KerasFeatureVector.from_vector(vect) for vect in vects]
+
+        else:
+            pop = PledgeEvolution.train_products(product_set, output_file, dataset)
+            last_population["products"] = last_population["products"] + pop
+            f1 = open(last_pdts_path, 'w')
+            f1.write(json.dumps([i.to_vector()
+                                for i in last_population["products"]]))
+            f1.close()
+
+        # list of (mutant_ratio,mutant_prds) sum of count_ratio should equal 1
+        mutant_ratios = [(1, 0.2),  (0.5, 0.2), (0.5, 0.2), (0.5, 0.2), (0.25, 0.2)]
+        #mutant_ratios = ((0.5, 1),)
+
+        for evo in range(evolution_epochs):
+            # The more we increase the epochs, the more we keep all the leaves
+            mutant_ratios[0] = (1, 0.2 +0.8*evo/(evolution_epochs-1))
+            mutant_ratios[1] = mutant_ratios[2] = mutant_ratios[3] =(0.5, 0.2*(evolution_epochs-evo)/evolution_epochs)
+
+            print("### evolution epoch {}".format(evo+last_evolution_epoch))
+            new_pop, new_pop_labels = PledgeEvolution.select(last_population, survival_count)
+            last_population["products"] = new_pop
+            print("### remaining top individuals {}".format(len(new_pop)))
+            
+            last_population = PledgeEvolution.evolve(survival_count,mutant_ratios, evo, session_path, new_pop_labels,input_file, nb_product_perparent, product_set, dataset, last_population  )
+            pop = sorted(last_population["products"],
+                        key=lambda x: x.accuracy, reverse=True)
+        
+            pdt_path = "{}/{}products_e{}.json".format(
+                session_path, nb_base_products, evo)
+            print("### remaining total individuals {} saved to {}. top accuracy: {}".format(
+                len(pop),pdt_path, pop[0].accuracy))
+            f1 = open(pdt_path, 'w')
+            f1.write(json.dumps([i.to_vector() for i in pop]))
+            f1.close()
 
 
 def main(argv):
@@ -247,7 +261,9 @@ def main(argv):
                                    "nb=","dataset=", "bpath=", "ifile=", "ofile=", "pfile="])
     except getopt.GetoptError:
         pass
+    print("arguments {}".format(opts))
     for opt, arg in opts:
+        
         if opt == '-h':
             print(
                 'evolution.py -nb <nb_architectures> -d <dataset> -b <base_path> -i <input_file> -o <output_file> -p <products_file>')
@@ -265,7 +281,7 @@ def main(argv):
         elif opt in ("-p", "--pfile"):
             products_file = arg
 
-    run(base, input_file, output_file,
+    PledgeEvolution.run(base, input_file, output_file,
         last_pdts_path=products_file, dataset=dataset, nb_base_products=nb_base_products)
 
 
