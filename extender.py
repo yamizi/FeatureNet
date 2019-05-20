090b696392d55c8929ad3e8f71835ea4498b2613
Salah GHAMIZI <salah.ghamizi@uni.lu>
2019-05-20 10:52:37 +0200

    Starting elements at 1 in loops

--- extender.py
+++ extender.py
@@ -1,9 +1,12 @@
 import xml.etree.ElementTree as ET
 
-nb_cells = 10
-nb_blocks = 1
+nb_cells = 5
+nb_blocks = 5
 
-tree = ET.parse("C:\\Users\salah.ghamizi\Desktop\main_1block_light.xml")
+input_file = "main_1block_nas.xml"
+output_file = "nas{}_{}.xml".format(nb_blocks,nb_cells)
+
+tree = ET.parse(input_file)
 root = tree.getroot()
 
 child = root.getchildren()
@@ -14,14 +17,14 @@ feature_tree_raw = feature_tree.text
 
 cellIndex = feature_tree_raw.find(":o Block[k]_Element[i]")
 cell = feature_tree_raw[cellIndex:]
-all_cells = "\t\t\t\t\t".join([cell.replace("[i]",str(i)) for i in range(nb_cells)])
+all_cells = "\t\t\t\t\t".join([cell.replace("[i]",str(i+1)) for i in range(nb_cells)])
 
 feature_tree_raw = feature_tree_raw[:cellIndex] + all_cells
 
 
 blockIndex = feature_tree_raw.find(":o Block[k](Block[k])")
 block = feature_tree_raw[blockIndex:]
-all_blocks = "\t\t\t\t".join([block.replace("[k]",str(k)) for k in range(nb_blocks)])
+all_blocks = "\t\t\t\t".join([block.replace("[k]",str(k+1)) for k in range(nb_blocks)])
 feature_tree_raw = feature_tree_raw[:blockIndex] + all_blocks
 
 feature_tree.text  = feature_tree_raw
@@ -45,7 +48,7 @@ for c in range(len(constraints_raw)):
                 continue
 
             if constraint[1].find("[i]")==-1:
-                replaced = constraint[1].replace("[k]", str(k)).replace("[k+1]", str(k+1))
+                replaced = constraint[1].replace("[k]", str(k+1)).replace("[k+1]", str(k+2))
                 #constraints_full ="{}\n{}{}0:{}".format(constraints_full, constraint[0], str(k), replaced)
                 constraints_full ="{}\nC{}:{}".format(constraints_full, constraint_id, replaced)
                 constraint_id = constraint_id+1
@@ -53,10 +56,10 @@ for c in range(len(constraints_raw)):
                 for i in range(nb_cells):
                     if constraint[1].find("[i+1]")!=-1 and i+1==nb_cells:
                         continue
-                    replaced = constraint[1].replace("[k]", str(k)).replace("[k+1]", str(k+1)).replace("[i]", str(i)).replace("[i+1]", str(i+1))
+                    replaced = constraint[1].replace("[k]", str(k+1)).replace("[k+1]", str(k+2)).replace("[i]", str(i+1)).replace("[i+1]", str(i+2))
                     #constraints_full ="{}\n{}{}{}:{}".format(constraints_full, constraint[0], str(k),i, replaced)
                     constraints_full ="{}\nC{}:{}".format(constraints_full, constraint_id, replaced)
                     constraint_id = constraint_id+1
 constraints.text = constraints_full+"\n"
 
-tree.write("C:\\Users\salah.ghamizi\Desktop\main_1block_10_cells.xml", encoding="UTF-8", xml_declaration=True)
+tree.write(output_file, encoding="UTF-8", xml_declaration=True)
