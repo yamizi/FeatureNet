a66541c33a66c4f2926623161318e679785e95f2
Salah GHAMIZI <salah.ghamizi@uni.lu>
2019-05-21 12:02:54 +0200

    updated configuration

--- job.sh
+++ job.sh
@@ -7,7 +7,7 @@
 #SBATCH --time=2-23:00:00
 #SBATCH --qos=qos-gpu
 #SBATCH -C skylake
-#SBATCH -J FeatureModel2Keras
+#SBATCH -J MNIST100-FeatureModel2Keras
 #SBATCH --mail-type=end,fail
 #SBATCH --mail-user=salah.ghamizi@uni.lu
 
@@ -19,6 +19,6 @@ module load math/Keras/2.1.6-foss-2018a-TensorFlow-1.8.0-Python-3.6.4
 module load lib/TensorFlow/1.8.0-foss-2018a-Python-3.6.4-CUDA-9.1.85
 pip install --upgrade --user  keras
 
-python -u pledge_evolution.py > bash_evol_pledge_cifar.out
+python -u pledge_evolution.py -d 'mnist' -b '../products/run8' > bash_evol_pledge_mnist_100.out
 # Your more useful application can be started below!
 #  dos2unix job.sh sbatch job.sh
