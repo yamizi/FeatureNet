f90cf9655ead8d25d52b64ec1941e96dafbd80bc
Salah GHAMIZI <salah.ghamizi@uni.lu>
2019-05-27 13:39:54 +0200

    Added command line parameters

--- full.py
+++ full.py
@@ -1,9 +1,8 @@
 
 from tensorflow_generator import TensorflowGenerator
 from products_tree import ProductSet
-import multiprocessing
 import json
-
+import sys, getopt, os
 
 
 
@@ -17,9 +16,11 @@ def run_tensorflow(product, url, index,datasets=[], epochs=12, depth=1, data_aug
                 f2.write("\r\n{0}: {1} {2} {3} {4} {5} {6}".format(index, tensorflow.accuracy, tensorflow.stop_training, tensorflow.training_time, tensorflow.params, tensorflow.flops, history))
                 f2.close()
 
-def main(target, min_index=0, max_index=0, filter_indices=[], datasets=None,epochs=12, depth=1, data_augmentation=False):
-        baseurl = "./"
-        productSet = ProductSet(baseurl+target+".pdt")
+def main(target, min_index=0, max_index=0, filter_indices=[], datasets=None,epochs=12, depth=1, data_augmentation=False, output_folder=""):
+        productSet = ProductSet(target+".pdt"
+        )
+        if not output_folder:
+                output_folder = "./products/"
 
         if not datasets:
                 datasets = ["mnist"]
@@ -28,27 +29,53 @@ def main(target, min_index=0, max_index=0, filter_indices=[], datasets=None,epoc
                 print("product {0}".format(index))
 
                 if index >= min_index and (len(filter_indices)==0 or index in filter_indices):
-                        f = open("{0}products/{1}_{2}.json".format(baseurl, target, index), "w")
+                        f = open("{0}{1}_{2}.json".format(output_folder, target, index), "w")
                         str_ = json.dumps(product)
                         f.write(str_)
                         f.close()
-
                         run_tensorflow(product, target, index, datasets, epochs, depth, data_augmentation=data_augmentation)
-                        #p = multiprocessing.Process(target=run_tensorflow, args=(product,))
-                        #p.start()
-                        #p.join()
 
                 if max_index!= 0 and index ==max_index:
                         break
 
-if __name__ == "__main__":
-    # execute only if run as a script
 
-    top_cifar = [59, 63, 143,  161, 203, 444, 477, 595, 634,  936]
-    #top_cifar = top_cifar[6:]
-    # small architectures < 4M on CIFAR
-    top_cifar = [59, 203, 477, 634, 63, 161, 936]
-    main("1000Products", datasets=["cifar"], epochs=600, depth=5, data_augmentation=False)
+
+def init(argv):
+    input_file = '1000Products'
+    depth=1
+    datasets = ["cifar"]
+    training_epochs = 600
+    output_folder = "./products/"
+
+    try:
+        opts, args = getopt.getopt(argv, "hi:n:d:t:", [
+                                   "ifile=", "depth=","datasets=","training_epoch="])
+    except getopt.GetoptError:
+        pass
+    print("arguments {}".format(opts))
+    for opt, arg in opts:
+        
+        if opt == '-h':
+            print(
+                'full.py -i <input_file> -n <depth> -d <datasets> -t <training_epoch>')
+            sys.exit()
+        elif opt in ("-n", "--depth"):
+            depth = int(arg)
+        elif opt in ("-d", "--dataset"):
+            datasets = arg.split(";")
+        elif opt in ("-i", "--ifile"):
+            input_file = arg
+        elif opt in ("-t", "--training_epoch"):
+            training_epochs = int(arg)
+
+    if not os.path.isdir(output_folder):
+        os.mkdir(output_folder)
+        
+    main(input_file, datasets=datasets, epochs=training_epochs, depth=depth, output_folder=output_folder)
+
+if __name__ == "__main__":
+    init(sys.argv[1:])
+
        
 
         
