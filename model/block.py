d9b600e70264217e77dcd09d562049ff82b78dcd
Salah GHAMIZI <salah.ghamizi@uni.lu>
2019-05-27 18:49:20 +0200

    Features & Stride support at the Block level

--- model/block.py
+++ model/block.py
@@ -9,6 +9,8 @@ class Block(Node):
 
         self.is_root = True
         self.cells = []
+        self._features = 0.0
+        self._stride = 0
         
         if previous_block:
             self.previous_block = previous_block
@@ -18,6 +20,13 @@ class Block(Node):
 
     def append_cell(self, cell):
         self.cells.append(cell)
+
+
+    def set_stride(self,stride_str):
+        self._stride = stride_str.split("x")
+
+    def set_features(self,features_str):
+        self._features = int(features_str)/100
 
     def get_custom_parameters(self):
         params = {}
@@ -68,15 +77,25 @@ class Block(Node):
         block = Block(raw_dict=feature_model)
         
         for cell_dict in feature_model.get("children"):
-            
-            if len(cell_dict.get("children")):
-                cell_type = cell_dict.get("children")[0].get("label")
-                cell_type = cell_type[cell_type.rfind("_")+1:]
-                cell_type = ''.join([i for i in cell_type if not i.isdigit()])
+            element_type = cell_dict.get("label")
+            element_type = element_type[element_type.rfind("_")+1:]
+            if element_type=="stride":
+                stride = cell_dict.get("children")[0].get("label")
+                block.set_stride(stride[stride.rfind("_")+1:])
+            elif element_type=="features":
+                feature = cell_dict.get("children")[0].get("label")
+                block.set_features(feature[feature.rfind("_")+1:])
+
+            elif len(cell_dict.get("children")):
+
                 
-                if cell_type=="Cell":
-                    cell = Cell.parse_feature_model(cell_dict.get("children")[0])
-                    block.cells.append(cell)
+                        cell_type = cell_dict.get("children")[0].get("label")
+                        cell_type = cell_type[cell_type.rfind("_")+1:]
+                        cell_type = ''.join([i for i in cell_type if not i.isdigit()])
+                
+                        if cell_type=="Cell":
+                            cell = Cell.parse_feature_model(cell_dict.get("children")[0])
+                            block.cells.append(cell)
         
         block.cells.sort(key = lambda a : a.get_name())
         return block
