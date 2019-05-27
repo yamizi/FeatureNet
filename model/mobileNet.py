70705b2f81ea75c1dde27c0509369b3e4e63d371
Salah GHAMIZI <salah.ghamizi@uni.lu>
2019-05-27 17:26:58 +0200

    MobileNet experimentation

---
+++ model/mobileNet.py
@@ -0,0 +1,13 @@
+from .input import Input, ZerosInput, PoolingInput, ConvolutionInput, DenseInput, IdentityInput
+from .output import Output, OutCell, OutBlock, Out
+from .operation import Operation, Combination, Sum, Flat
+from .block import Block
+from .cell import Cell
+
+
+
+def mobilenet_blocks():
+    blocks = []
+
+    
+    return blocks
