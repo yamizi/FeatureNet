f2ec94db4ede46143a10aa6cc495f40f31f81295
Salah GHAMIZI <salah.ghamizi@uni.lu>
2019-06-25 17:08:46 +0200

    External Job runner to handle cmd parameters

---
+++ job_runner.py
@@ -0,0 +1,65 @@
+from model.mutation.mutable_base import MutableBase, MutationStrategies
+import getopt, sys
+
+base_path = '../products'
+base_training_epochs = 12
+
+
+def main(argv):
+    input_file = ''
+    output_file = ''
+    products_file = ''
+    base = base_path
+    nb_base_products=[20]
+    dataset = "cifar"
+    training_epochs = base_training_epochs
+    mutation_rate = 0.1
+    survival_rate = 0.1
+    breed = True
+    evolution_epochs = 50
+
+    MutableBase.MAX_NB_CELLS = 5
+    MutableBase.MAX_NB_BLOCKS = 10
+
+    MutableBase.mutation_stategy = MutationStrategies.CHOICE
+    opts = []
+    try:
+        opts, args = getopt.getopt(argv, "hn:d:b:p:t:m:r:s:e:", [
+                                   "nb=","dataset=", "bpath=", "pfile=", "training_epoch=", "mutation_strategy=", "mutation_rate=", "survival_rate=", "evolution_epoch="])
+        
+    except getopt.GetoptError:
+        print("no arguments")
+        pass
+
+    print("arguments {}".format(opts))
+    for opt, arg in opts:
+        
+        if opt == '-h':
+            print(
+                'pledge_evolution.py -n <nb_architectures> -d <dataset> -b <base_path> -p <products_file> -t <training_epoch> -m <mutation_strategy> -r <mutation_rate> -s <survival_rate>')
+            sys.exit()
+        elif opt in ("-n", "--nb"):
+            nb_base_products = arg.split("x")
+        elif opt in ("-d", "--dataset"):
+            dataset = arg
+        elif opt in ("-b", "--bpath"):
+            base = arg
+        elif opt in ("-p", "--pfile"):
+            products_file = arg
+        elif opt in ("-t", "--training_epoch"):
+            training_epochs = int(arg)
+        elif opt in ("-m", "--mutation_strategy"):
+            if arg=="all":
+                MutableBase.mutation_stategy = MutationStrategies.ALL
+        elif opt in ("-r", "--mutation_rate"):
+            mutation_rate = float(arg)
+        elif opt in ("-s", "--survival_rate"):
+            survival_rate = float(arg)
+        elif opt in ("-e", "--evolution_epoch"):
+            evolution_epochs = int(arg)
+
+    from full_evolution import FullEvolution
+    FullEvolution.run(base, last_pdts_path=products_file, dataset=dataset, nb_base_products=int(nb_base_products[0]), training_epochs=training_epochs, mutation_rate=mutation_rate,survival_rate=survival_rate, breed=breed, evolution_epochs=evolution_epochs)
+
+if __name__ == "__main__":
+    main(sys.argv[1:])
