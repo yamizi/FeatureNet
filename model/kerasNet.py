f18f74611efd6d366af8cf25b42635818275b5c9
Salah GHAMIZI <salah.ghamizi@uni.lu>
2020-06-22 17:31:54 +0200

    Added LSTM & CNN FeatureModel representation

--- model/kerasNet.py
+++ model/kerasNet.py
@@ -1,6 +1,5 @@
-from .input import Input, ZerosInput, PoolingInput, ConvolutionInput, DenseInput, IdentityInput
-from .output import Output, OutCell, OutBlock, Out
-from .operation import Operation, Combination, Sum, Flat, Drop
+from .input import EmbeddingInput, DenseInput, LSTMInput, PoolingInput, ConvolutionInput, IdentityInput
+from .operation import  Drop, Active
 from .block import Block
 from .cell import Cell
 
@@ -31,3 +30,65 @@ def standard_blocks():
     blocks.extend([block1, block2,block4])
 
     return blocks
+
+
+def lstm_blocks(max_features = 20000,lstm_units = 128):
+    blocks = []
+
+    block1 = Block()
+
+    cell11 = Cell(input1 = EmbeddingInput(max_features))
+    block1.append_cell(cell11)
+    cell12 = Cell(input1 = LSTMInput(lstm_units))
+    block1.append_cell(cell12)
+
+    block2 = Block()
+    block2.set_stride("1x1")
+    cell21 = Cell(input1 = DenseInput(1, "sigmoid"))
+    block2.append_cell(cell21)
+
+    blocks.extend([block1, block2])
+
+def lstm_blocks(max_features = 20000,lstm_units = 128):
+    blocks = []
+
+    block1 = Block()
+    cell11 = Cell(input1 = EmbeddingInput(max_features))
+    block1.append_cell(cell11)
+
+    block2 = Block()
+    cell21 = Cell(input1 = LSTMInput(lstm_units))
+    block2.append_cell(cell21)
+
+    block3 = Block()
+    cell31 = Cell(input1 = DenseInput(1, "sigmoid"))
+    block3.append_cell(cell31)
+
+    blocks.extend([block1, block2, block3])
+
+
+def cnn1d_blocks(input_dims = 20000,maxlen = 400,output_dims = 50,filters = 250,hidden_dims = 250):
+    blocks = []
+
+    block1 = Block()
+
+    cell11 = Cell(input1 = EmbeddingInput(input_dims=input_dims,_output_dim=output_dims, _input_length=maxlen))
+    block1.append_cell(cell11)
+
+    block2 = Block()
+    block2.set_stride("1x1")
+    cell21 = Cell(input1 = ConvolutionInput(_features=filters,_activation="none"))
+    block2.append_cell(cell21)
+    cell22 = Cell(input1=PoolingInput(_features=2, _stride="none"))
+    block2.append_cell(cell22)
+    cell23 = Cell(input1=DenseInput(hidden_dims, "none"), operation1=Drop(20))
+    block2.append_cell(cell23)
+    cell24 = Cell(input1=IdentityInput(), operation1=Active("relu"))
+    block2.append_cell(cell24)
+
+    block3 = Block()
+    block3.set_stride("1x1")
+    cell31 = Cell(input1=DenseInput(1, "sigmoid"))
+    block3.append_cell(cell31)
+
+    blocks.extend([block1, block2, block3])
--- model/leNet.py
+++ model/leNet.py
@@ -1,6 +1,4 @@
-from .input import Input, ZerosInput, PoolingInput, ConvolutionInput, DenseInput, IdentityInput
-from .output import Output, OutCell, OutBlock, Out
-from .operation import Operation, Combination, Sum, Flat
+from .input import PoolingInput, ConvolutionInput, DenseInput
 from .block import Block
 from .cell import Cell
 
